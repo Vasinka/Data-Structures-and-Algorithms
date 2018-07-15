9a936e9bc240aaf2b5fe15f9aae5d2ba3c79fb1a
Vasinka <41026837+Vasinka@users.noreply.github.com>
2018-07-15 16:18:51 +0300

    Add files via upload

---
+++ Stack/Binary.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main ()
+{
+    int number;
+
+    cin >> number;
+
+    stack <int> st;
+
+    while (number)    // делим number целочислено на 2 и записваме остатъка от делението в стека
+    {
+        st.push(number%2);
+
+        number /= 2;
+    }
+
+    while (!st.empty()) // извеждаме елементите на стека един по един
+    {                   //така напрактика имаме на стандартния изход number в двоичния му запис
+        cout << st.top();
+
+        st.pop();
+    }
+
+    cout << endl;
+
+    return 0;
+}
