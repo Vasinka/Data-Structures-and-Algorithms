d48f10888a64d686c23e50b70cf1620afa1aacf9
Vasilena Kaludova <41026837+Vasinka@users.noreply.github.com>
2018-10-25 01:16:24 +0300

    Add files via upload

---
+++ binarySearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int x)
+{
+    while (left <= right)
+    {
+        int mid = left + (right-left)/2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] < x)
+            left = mid + 1;
+
+        else
+            right = mid - 1;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 4, 10, 10};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    int x = 10;
+    cout << binarySearch(arr, 0, n-1, x) << endl;
+
+    return 0;
+}
